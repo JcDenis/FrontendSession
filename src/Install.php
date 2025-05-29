c2d3c3e74c6e169716cbbc9c8c7b360b95cc1507
Philippe <philippe.mail.perso@gmail.com>
2025-05-29 06:45:38 +0200

    Update Install.php
    Typos

--- src/Install.php
+++ src/Install.php
@@ -33,13 +33,13 @@ class Install extends Process
             My::settings()->put('enable_registration', false, 'boolean', 'Enable user registration form on frontend', false, true);
             My::settings()->put('enable_recovery', false, 'boolean', 'Enable user password recovery form on frontend', false, true);
             My::settings()->put('limit_comment', false, 'boolean', 'Limit new comments to registered users', false, true);
-            My::settings()->put('condition_page', '', 'text', 'Use link to terms and conditions page on singup form', false, true);
+            My::settings()->put('condition_page', '', 'text', 'Use link to terms and conditions page on signup form', false, true);
             My::settings()->put('disable_css', false, 'boolean', 'Disable default CSS', false, true);
             My::settings()->put('log_form_error', false, 'boolean', 'Log frontend forms submissions errors', false, true);
             My::settings()->put('email_registration', '', 'text', 'Email to send registration confirmation to', false, true);
             My::settings()->put('email_from', '', 'text', 'No-reply email address for confirmation mail', false, true);
             My::settings()->put('connected', "You're now connected to the blog.", 'text', 'Connected display text', false, true);
-            My::settings()->put('disconnected', "You're must be connected to unlock all blog's features.", 'text', 'Diconnected display text', false, true);
+            My::settings()->put('disconnected', "You must be connected to unlock all blog's features.", 'text', 'Disconnected display text', false, true);
 
             return true;
         } catch (Exception $e) {
