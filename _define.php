5752e369f7da8c50e30bedf2d3d42ced45cd3374
Jean-Christian Denis <contact@jcdenis.fr>
2025-10-18 17:06:23 +0200

    release 0.35.2

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Frontend sessions',
     'Allow session on frontend.',
     'Jean-Christian Paul Denis and Contributors',
-    '0.35.1',
+    '0.35.2',
     [
         'requires'    => [['core', '2.36']],
         'settings'    => ['blog' => '#params.' . $this->id . '_params'],
